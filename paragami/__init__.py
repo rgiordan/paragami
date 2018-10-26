c7f1a39255f5800cca436ee3bd5e90ac827ef52e
Ryan Giordano <rgiordan@gmail.com>
2018-10-26 14:14:50 -0700

    move containers to own module

--- paragami/__init__.py
+++ paragami/__init__.py
@@ -1,4 +1,4 @@
-from paragami.base_patterns import PatternDict, PatternArray
+from paragami.pattern_containers import PatternDict, PatternArray
 from paragami.numeric_array_patterns import NumericArrayPattern
 from paragami.pdmatrix_patterns import PDMatrixPattern
 from paragami.function_patterns import FlattenedFunction
