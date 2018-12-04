29226dfbd1fb5fef7c038c840bb53e64c1a9fbab
Ryan Giordano <rgiordan@gmail.com>
2018-12-03 16:27:15 -0800

    Don't do multi-dim slogdet

--- paragami/autograd_supplement_lib.py
+++ paragami/autograd_supplement_lib.py
@@ -12,15 +12,21 @@ def inv_jvp(g, ans, x):
 
 defjvp(inv, inv_jvp)
 
-def solve_jvp_0(g, ans, x, y):
+def solve_jvp_argnum0(g, ans, x, y):
     return -1 * np.linalg.solve(x, g) @ ans
 
-def solve_jvp_1(g, ans, x, y):
+def solve_jvp_argnum1(g, ans, x, y):
     return np.linalg.solve(x, g)
 
-defjvp(solve, solve_jvp_0, solve_jvp_1)
+defjvp(solve, solve_jvp_argnum0, solve_jvp_argnum1)
 
 def slogdet_jvp(g, ans, x):
+    # Due to https://github.com/HIPS/autograd/issues/115
+    # and https://github.com/HIPS/autograd/blob/65c21e/tests/test_numpy.py#L302
+    # it does not seem easy to take the trace of the last two dimensions of
+    # a multi-dimensional array at this time.
+    if len(x.shape) > 2:
+        raise ValueError('JVP is only supported for 2d input.')
     return 0, np.trace(np.linalg.solve(x.T, g.T))
 
 defjvp(slogdet, slogdet_jvp)
--- paragami/tests/test_autograd_supplement_lib.py
+++ paragami/tests/test_autograd_supplement_lib.py
@@ -12,6 +12,11 @@ import unittest
 npr.seed(1)
 
 
+def rand_psd(D):
+    mat = npr.randn(D, D)
+    return np.dot(mat, mat.T)
+
+
 class TestAutogradSupplement(unittest.TestCase):
     def test_inv(self):
         def fun(x):
@@ -22,28 +27,70 @@ class TestAutogradSupplement(unittest.TestCase):
 
         check_grads(fun)(mat)
 
-    def test_solve(self):
-        def fun(x, y):
-            return np.linalg.solve(x, y)
+    def test_inv_3d(self):
+        fun = lambda x: np.linalg.inv(x)
 
-        D = 3
-        mat1 = npr.randn(D, D) + np.eye(D) * 2
-        mat2 = npr.randn(D, D)
+        D = 4
+        mat = npr.randn(D, D, D) + 5 * np.eye(D)
+        check_grads(fun)(mat)
 
-        check_grads(fun)(mat1, mat2)
+        mat = npr.randn(D, D, D, D) + 5 * np.eye(D)
+        check_grads(fun)(mat)
 
     def test_slogdet(self):
         def fun(x):
             sign, logdet = np.linalg.slogdet(x)
             return logdet
 
-        D = 3
+        D = 6
         mat = npr.randn(D, D)
-        mat[0, 0] += 1  # Make sure the matrix is not symmetric
+        mat[0, 1] = mat[1, 0] + 1  # Make sure the matrix is not symmetric
 
         check_grads(fun)(mat)
         check_grads(fun)(-mat)
 
+    def test_slogdet_3d(self):
+        # At this time, this is not supported.
+        pass
+        # fun = lambda x: np.sum(np.linalg.slogdet(x)[1])
+        # mat = np.concatenate(
+        #     [(rand_psd(5) + 5 * np.eye(5))[None,...] for _ in range(3)])
+        # check_grads(fun)(mat)
+
+    def test_solve_arg1(self):
+        D = 8
+        A = npr.randn(D, D) + 10.0 * np.eye(D)
+        B = npr.randn(D, D - 1)
+        def fun(a): return np.linalg.solve(a, B)
+        check_grads(fun)(A)
+
+    def test_solve_arg1_1d(self):
+        D = 8
+        A = npr.randn(D, D) + 10.0 * np.eye(D)
+        B = npr.randn(D)
+        def fun(a): return np.linalg.solve(a, B)
+        check_grads(fun)(A)
+
+    def test_solve_arg2(self):
+        D = 6
+        A = npr.randn(D, D) + 1.0 * np.eye(D)
+        B = npr.randn(D, D - 1)
+        def fun(b): return np.linalg.solve(A, b)
+        check_grads(fun)(B)
+
+    def test_solve_arg1_3d(self):
+        D = 4
+        A = npr.randn(D+1, D, D) + 5*np.eye(D)
+        B = npr.randn(D+1, D)
+        fun = lambda A: np.linalg.solve(A, B)
+        check_grads(fun)(A)
+
+    def test_solve_arg1_3d_3d(self):
+        D = 4
+        A = npr.randn(D+1, D, D) + 5*np.eye(D)
+        B = npr.randn(D+1, D, D+2)
+        fun = lambda A: np.linalg.solve(A, B)
+        check_grads(fun)(A)
 
 if __name__ == '__main__':
     unittest.main()
