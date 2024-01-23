f79cf021fa6c4c5bfe7de877b0b8058e0eeb073c
Ryan Giordano <rgiordan@gmail.com>
2024-01-23 15:54:56 -0800

    function tests

--- paragami/tests/test.py
+++ paragami/tests/test.py
@@ -17,11 +17,6 @@ def PytreesL1(p1, p2):
     return jtu.tree_reduce(lambda x, y: x + y, diff_tree, 0.0)
 
 
-def TestBijector(par, par_bij, tol=1e-8):
-    par_free = paragami.Unconstrain(par, par_bij)
-    par2 = paragami.Constrain(par_free, par_bij)
-    err = PytreesL1(par, par2)
-    assert(err < tol)
 
 def GetSymMat(dim, rng_key):
     a = jax.random.uniform(rng_key, (dim, dim))
@@ -29,14 +24,7 @@ def GetSymMat(dim, rng_key):
     return a
 
 
-
-class TestParagami(unittest.TestCase):
-    x = jnp.array([1.0])
-    assert_array_almost_equal(x, x)
-
-
-class TestBijector(unittest.TestCase):
-
+def GetTestParameter():
     # Don't do flaky tests
     rng_key = jax.random.key(42)
 
@@ -47,14 +35,45 @@ class TestBijector(unittest.TestCase):
     rng_key, rng_key2 = jax.random.split(rng_key)
     par['b']['b2'] = GetSymMat(3, rng_key2) + jnp.eye(3)
 
+    return par
 
+
+def GetTestBijector(par):
     par_bij = paragami.GetDefaultBijectors(par)
     par_bij['a'] = tfp.bijectors.Exp()
     par_bij['b']['b2'] = tfp.bijectors.CholeskyOuterProduct()
+    return par_bij
 
-    par_free = paragami.Unconstrain(par, par_bij)
-    par2, ldj = paragami.Constrain(par_free, par_bij, compute_log_det_jac=True)
-    TestBijector(par, par_bij)
+
+
+
+
+class TestBijector(unittest.TestCase):
+    def _CheckBijector(self, par, par_bij, tol=1e-8):
+        par_free = paragami.Unconstrain(par, par_bij)
+        par2 = paragami.Constrain(par_free, par_bij)
+        err = PytreesL1(par, par2)
+        self.assertTrue(err < tol)
+
+    def test_constraints(self):
+        par = GetTestParameter()
+        par_bij = GetTestBijector(par)
+        par_free = paragami.Unconstrain(par, par_bij)
+        par2, ldj = paragami.Constrain(par_free, par_bij, compute_log_det_jac=True)
+        self._CheckBijector(par, par_bij)
+
+    def test_functions(self):
+        par = GetTestParameter()
+        par_bij = GetTestBijector(par)
+
+        def Fun(par, x, y):
+            return x * par['a'] + y * jnp.sum(par['b']['b1']) + \
+                jnp.sum(jnp.linalg.eigvalsh(par['b']['b2']))
+
+        FreeFun = paragami.UnconstrainObjective(Fun, par_bij)
+
+        free_par = paragami.Unconstrain(par, par_bij)
+        self.assertTrue(FreeFun(free_par, 2, y=3) == Fun(par, y=3, x=2))
 
 
 if __name__ == '__main__':
